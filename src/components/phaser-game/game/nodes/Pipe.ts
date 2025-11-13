fcf25deda47e014e7528b9a0bf2df818a5f98ede
MaakCode <11623298+maakcode@users.noreply.github.com>
2025-11-14 01:19:20 +0900

    파이프 추가, 스코어 설정

---
+++ src/components/phaser-game/game/nodes/Pipe.ts
@@ -0,0 +1,87 @@
+import Phaser from "phaser";
+
+export class Pipe extends Phaser.GameObjects.Container {
+  stick: Phaser.GameObjects.Sprite;
+  head: Phaser.GameObjects.Sprite;
+
+  constructor(scene: Phaser.Scene, x: number, y: number, reversed = false) {
+    super(scene, x, y);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+
+    this.head = scene.add.sprite(0, 0, "head_wood").setOrigin(0.5, 0);
+    this.stick = scene.add
+      .sprite(0, this.head.displayHeight, "stick_wood")
+      .setOrigin(0.5, 0);
+    this.stick.scaleY = 3;
+
+    this.add([this.stick, this.head]);
+
+    if (this.body instanceof Phaser.Physics.Arcade.Body) {
+      this.body.allowGravity = false;
+      this.body.immovable = true;
+      this.body.setSize(
+        this.head.displayWidth,
+        this.head.displayHeight + this.stick.displayHeight
+      );
+      this.body.setOffset(-0.5 * this.body.width, 0);
+
+      if (reversed) {
+        this.angle = 180;
+        this.body.setOffset(-0.5 * this.body.width, -this.body.height);
+      }
+    }
+  }
+
+  setLevel(level: number) {
+    if (this.body instanceof Phaser.Physics.Arcade.Body) {
+      this.body.setVelocityX(-200 - 5 * level);
+    }
+  }
+
+  preUpdate() {
+    if (this.x + this.width < this.scene.cameras.main.worldView.left) {
+      this.destroy();
+    }
+  }
+
+  destroy(fromScene?: boolean) {
+    super.destroy(fromScene);
+  }
+}
+
+export class ScoreArea extends Phaser.GameObjects.Container {
+  area!: Phaser.GameObjects.Rectangle;
+
+  constructor(scene: Phaser.Scene, x: number, y: number, gap: number) {
+    super(scene, x, y);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+
+    this.area = scene.add.rectangle(0, 0, 6, gap).setOrigin(0.5, 0.5);
+    this.add([this.area]);
+
+    if (this.body instanceof Phaser.Physics.Arcade.Body) {
+      this.body.allowGravity = false;
+      this.body.immovable = true;
+      this.body.setSize(this.area.displayWidth, this.area.displayHeight);
+      this.body.setOffset(-0.5 * this.body.width, -0.5 * this.body.height);
+    }
+  }
+
+  setLevel(level: number) {
+    if (this.body instanceof Phaser.Physics.Arcade.Body) {
+      this.body.setVelocityX(-200 - 5 * level);
+    }
+  }
+
+  preUpdate() {
+    if (this.x + this.width < this.scene.cameras.main.worldView.left) {
+      this.destroy();
+    }
+  }
+
+  destroy(fromScene?: boolean) {
+    super.destroy(fromScene);
+  }
+}
--- src/components/phaser-game/game/scenes/Game.ts
+++ src/components/phaser-game/game/scenes/Game.ts
@@ -1,10 +1,15 @@
-import { EventBus } from "../EventBus";
 import { Scene } from "phaser";
+import { EventBus } from "../EventBus";
 import { Duck } from "../nodes/Duck";
+import { Pipe, ScoreArea } from "../nodes/Pipe";
 
 export class Game extends Scene {
-  duck!: Phaser.GameObjects.Image;
+  score = 0;
+  duck!: Duck;
   scoreText!: Phaser.GameObjects.Text;
+  pipeColliderGroup!: Phaser.GameObjects.Group;
+  scoreColliderGroup!: Phaser.GameObjects.Group;
+  spawnEvent!: Phaser.Time.TimerEvent;
 
   constructor() {
     super("Game");
@@ -13,6 +18,32 @@ export class Game extends Scene {
   create() {
     const { width, height } = this.scale;
     this.duck = new Duck(this, 0.2 * width, 0.5 * height);
+
+    this.pipeColliderGroup = this.add.group();
+    this.scoreColliderGroup = this.add.group();
+
+    this.spawnEvent = this.time.addEvent({
+      delay: 1800,
+      callback: this.addPipeRow,
+      callbackScope: this,
+      loop: true,
+    });
+
+    this.physics.add.collider(
+      this.duck,
+      this.pipeColliderGroup,
+      this.changeScene,
+      undefined,
+      this
+    );
+
+    this.physics.add.collider(
+      this.duck,
+      this.scoreColliderGroup,
+      this.addScore,
+      undefined,
+      this
+    );
 
     this.scoreText = this.add
       .text(0.5 * width, 0.15 * height, "0", {
@@ -27,6 +58,50 @@ export class Game extends Scene {
       .setDepth(100);
 
     EventBus.emit("current-scene-start", this);
+  }
+
+  private addPipeRow() {
+    const { width, height } = this.scale;
+
+    const gap = 250;
+    const x = width + 50;
+    const y = Phaser.Math.Between(0.5 * gap + 20, height - 0.5 * gap - 20);
+
+    const bottomPipe = new Pipe(this, x, y + 0.5 * gap);
+    const scoreArea = new ScoreArea(this, x, y, gap);
+    const topPipe = new Pipe(this, x, y - 0.5 * gap, true);
+
+    bottomPipe.setLevel(this.score);
+    scoreArea.setLevel(this.score);
+    topPipe.setLevel(this.score);
+
+    this.pipeColliderGroup.add(bottomPipe);
+    this.scoreColliderGroup.add(scoreArea);
+    this.pipeColliderGroup.add(topPipe);
+  }
+
+  private addScore(object1: unknown, object2: unknown) {
+    if (object1 instanceof ScoreArea) {
+      object1.destroy();
+    } else if (object2 instanceof ScoreArea) {
+      object2.destroy();
+    }
+
+    this.score++;
+    this.scoreText.setText(this.score.toString());
+    this.spawnEvent.timeScale = 1 + 0.02 * this.score;
+    this.pipeColliderGroup.children.iterate((child) => {
+      if (child instanceof Pipe) {
+        child.setLevel(this.score);
+      }
+      return true;
+    });
+    this.scoreColliderGroup.children.iterate((child) => {
+      if (child instanceof ScoreArea) {
+        child.setLevel(this.score);
+      }
+      return true;
+    });
   }
 
   changeScene() {
